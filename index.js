dd5263da3855f002f0959637606ea49cfc58e892
Elijah Ngocho <elijahngocho24@gmail.com>
2023-09-19 11:56:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName = 'Scuber';//companyName is set as Scuber; is defined as a const
+
+let mostProfitableNeighborhood = 'Chelsea';// mostProfitableNeighborhood is declared as equal to Chelsea; is defined using let
+
+let companyCeo = 'Susan Smith'; //companyCeo is declared as equal to Susan Smith; is defined using let
+
+
+
+
